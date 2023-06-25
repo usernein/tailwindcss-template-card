32e43853abca9fdee6b3aa7aff4ce04402c583bb
Pauxis <pauxiscezar@gmail.com>
2023-06-24 23:01:05 -0300

    feat: add padding to textarea

--- src/components/TextareaEditor/index.tsx
+++ src/components/TextareaEditor/index.tsx
@@ -9,21 +9,23 @@ export function TextareaEditor ({
 }) {
   let timeoutPointer: NodeJS.Timeout
   return (
-    <textarea
-      value={value}
-      onInput={e => {
-        if (timeoutPointer) {
-          clearTimeout(timeoutPointer)
-        }
-        const value = (e.target as HTMLInputElement).value
+    <div className='p-2'>
+      <textarea
+        value={value}
+        onInput={e => {
+          if (timeoutPointer) {
+            clearTimeout(timeoutPointer)
+          }
+          const value = (e.target as HTMLInputElement).value
 
-        timeoutPointer = setTimeout(() => {
-          onChange(value)
-        }, debounceChangePeriod)
-      }}
-      class='textarea textarea-accent h-48 font-mono rounded-xl w-full leading-4'
-      placeholder={`<div class=''></div>`}
-      spellcheck={false}
-    ></textarea>
+          timeoutPointer = setTimeout(() => {
+            onChange(value)
+          }, debounceChangePeriod)
+        }}
+        class='textarea h-48 font-mono rounded-xl w-full leading-4'
+        placeholder={`<div class=''></div>`}
+        spellcheck={false}
+      ></textarea>
+    </div>
   )
 }
